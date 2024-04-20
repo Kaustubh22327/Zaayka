15512a4521b69cec83af9fc20c263e45e09aee6c
kaustubh0504 <kaustubh0504@gmail.com>
2024-04-20 22:37:14 +0530

    menu-list-item-added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 import "./Home.css";
 import Header from "../../components/Header/Header";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
+import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
 
 const Home = () => {
-  const [category,setCategory]=useState("All");
+  const [category, setCategory] = useState("All");
   return (
     <>
       <Header></Header>
       <ExploreMenu category={category} setCategory={setCategory}></ExploreMenu>
+      <FoodDisplay></FoodDisplay>
     </>
   );
 };
