7b9ebcd08173c56388d0d37f7e1626863d632f2a
kaustubh0504 <kaustubh0504@gmail.com>
2024-04-21 13:16:33 +0530

    responsiveness added

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,18 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Cart from "./Pages/cart/Cart";
 import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
-    <div className="app">
-      <Navbar />
-      <Home />
-    </div>
+    <>
+      <div className="app">
+        <Navbar />
+        <Home />
+      </div>
+      <Footer></Footer>
+
+    </>
   );
 };
 
